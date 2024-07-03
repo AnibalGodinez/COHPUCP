2046ad9ed526c80040cf726ed503046eab25ba7b
Anibal Johan Godinez Cortez <111099889+AnibalGodinez@users.noreply.github.com>
2024-07-03 10:01:08 -0600

    Quitar barra Lateral

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
 
-<div class="info-section text-center" style="margin: -400px; background-image: url('{{ asset('white/img/photo.webp') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 100vh;">
+<div class="info-section text-center" style="margin: -400px; background-image: url('{{ asset('white/img/fondo4.webp') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 100vh;">
     <div class="header-body text-center mb-7">
-        <div class="col-md-5 mx-auto">
+        <div class="col-md-5 mx-auto"><br><br><br><br>      
             <div class="info-area info-horizontal mt-80" style="margin-top: 250px">
-                <div class="description"><br>
+                <!-- Imagen antes de la Misión -->
+                <img src="{{ asset('white/img/favicon.png') }}" alt="Descripción de la imagen" style="max-width: 100%; height: auto; margin-bottom: 20px;">
+                <div class="description"><br><br>
                     <h3 class="info-title">Misión</h3>
                     <p class="description">
                         Impulsar y promover la superación de nuestros agremiados; velando por la protección de los derechos inherentes como profesionales de la Contaduría Pública; así como, garantizar que el ejercicio de la profesión se desarrolle de acuerdo a los estándares éticos reconocidos a nivel mundial. 
