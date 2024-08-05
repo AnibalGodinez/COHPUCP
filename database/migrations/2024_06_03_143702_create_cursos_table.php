72e515c7cc733c6300856a9f49fbc1d36c2712ea
Anibal Johan Godinez Cortez <111099889+AnibalGodinez@users.noreply.github.com>
2024-08-05 16:56:04 -0600

    Crear tablas Idiomas y categorías

--- database/migrations/2024_06_03_143702_create_cursos_table.php
+++ database/migrations/2024_06_03_143702_create_cursos_table.php
@@ -10,20 +10,34 @@ class CreateCursosTable extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
+            $table->string('layout');
             $table->string('nombre');
             $table->string('descripcion');
-            $table->decimal('precio', 8, 2); // Campo para el precio del curso (2 decimales)
+            $table->string('categoria');
+            $table->string('precio'); // Campo para el precio del curso, es string porque puede agregar 'Gratis'
             $table->text('enlace')->nullable(); // Campo opcional para un enlace relacionado con el curso
             $table->decimal('calificacion', 3, 1)->nullable(); // Campo opcional para la calificación del curso (1 decimal)
             $table->string('idioma')->nullable(); // Campo opcional para el idioma del curso
             $table->string('imagen')->nullable(); // Campo opcional para la imagen del curso
             $table->unsignedBigInteger('user_id'); // Campo para relacionar con la tabla de usuarios
+            $table->unsignedBigInteger('idioma_id')->nullable(); // Nuevo campo para relacionar con la tabla de idiomas
+            $table->unsignedBigInteger('categoria_id')->nullable(); // Nuevo campo para relacionar con la tabla de categorías
             $table->timestamps();
         
             // Define la restricción de clave externa con acciones en cascada
             $table->foreign('user_id')
                   ->references('id')
                   ->on('users')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreign('idioma_id')
+                  ->references('id')
+                  ->on('idiomas')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreign('categoria_id')
+                  ->references('id')
+                  ->on('categorias')
                   ->onUpdate('cascade')
                   ->onDelete('cascade');
         });
