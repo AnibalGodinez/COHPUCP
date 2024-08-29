52c4e9621a6fb24023182bf1043f881a9cce1412
Anibal Johan Godinez Cortez <111099889+AnibalGodinez@users.noreply.github.com>
2024-08-29 09:49:31 -0600

    Registro de usuarios | Corregido el error

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -23,52 +23,52 @@ class RegisterController extends Controller
     }
 
     protected function validator(array $data)
-{
-    // Obtener el año actual
-    $yearNow = Carbon::now()->year;
-    // Calcular los años mínimo y máximo permitidos
-    $yearMin = $yearNow - 106;
-    $yearMax = $yearNow - 18;
+    {
+        // Obtener el año actual
+        $yearNow = Carbon::now()->year;
+        // Calcular los años mínimo y máximo permitidos
+        $yearMin = $yearNow - 106;
+        $yearMax = $yearNow - 18;
 
-    return Validator::make($data, [
-        'name' => ['required', 'string', 'max:255'],
-        'name2' => ['nullable', 'string', 'max:255'],
-        'apellido' => ['required', 'string', 'max:255'],
-        'apellido2' => ['nullable', 'string', 'max:255'],
-        'numero_identidad' => ['required', 'string', 'unique:users,numero_identidad'],
-        'numero_colegiacion' => ['nullable', 'string', 'unique:users,numero_colegiacion'],
-        'rtn' => ['nullable', 'string', 'max:20', 'unique:users,rtn'],
-        'sexo' => ['required', 'in:masculino,femenino'],
-        'fecha_nacimiento' => [
-            'required',
-            'date',
-            function ($attribute, $value, $fail) use ($yearMin, $yearMax) {
-                $birthYear = Carbon::parse($value)->year;
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'name2' => ['nullable', 'string', 'max:255'],
+            'apellido' => ['required', 'string', 'max:255'],
+            'apellido2' => ['nullable', 'string', 'max:255'],
+            'numero_identidad' => ['required', 'string', 'unique:users,numero_identidad'],
+            'numero_colegiacion' => ['nullable', 'string', 'unique:users,numero_colegiacion'],
+            'rtn' => ['nullable', 'string', 'max:20', 'unique:users,rtn'],
+            'sexo_id' => ['nullable', 'exists:sexo,id'],
+            'fecha_nacimiento' => [
+                'required',
+                'date',
+                function ($attribute, $value, $fail) use ($yearMin, $yearMax) {
+                    $birthYear = Carbon::parse($value)->year;
 
-                if ($birthYear > $yearMax) {
-                    $fail('Debe tener al menos 18 años para registrarse.');
-                } elseif ($birthYear < $yearMin) {
-                    $fail('La fecha de nacimiento debe estar dentro del rango de 106 años.');
-                }
-            },
-        ],
-        'telefono' => ['nullable', 'string', 'max:15', 'regex:/^\d{4}-\d{4}$/'],
-        'telefono_celular' => ['required', 'string', 'max:15', 'regex:/^\d{4}-\d{4}$/'],
-        'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email', 'confirmed'],
-        'password' => ['required', 'string', 'min:8', 'confirmed', 'regex:/^(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/'],
-        'pais_id' => ['nullable', 'exists:pais,id'],
-        'agree_terms_and_conditions' => ['required'],
-    ], [
-        'numero_identidad.unique' => 'Este número de identidad ya se encuentra registrado',
-        'numero_colegiacion.unique' => 'Este número de colegiación ya se encuentra registrado',
-        'rtn.unique' => 'Este RTN ya se encuentra registrado',
-        'email.unique' => 'Este correo electrónico ya se encuentra registrado',
-        'email.confirmed' => 'La confirmación del correo electrónico no coincide',
-        'password.regex' => 'La contraseña debe contener al menos un símbolo o carácter especial, como por ejemplo: ^?=.,[]{}()!@#$%^&*"|<:>\ ',
-        'telefono.regex' => 'El número de teléfono fijo debe contener solo números y guiones',
-        'telefono_celular.regex' => 'El número de celular debe contener solo números y guiones',
-    ]);
-}
+                    if ($birthYear > $yearMax) {
+                        $fail('Debe tener al menos 18 años para registrarse.');
+                    } elseif ($birthYear < $yearMin) {
+                        $fail('La fecha de nacimiento debe estar dentro del rango de 106 años.');
+                    }
+                },
+            ],
+            'telefono' => ['nullable', 'string', 'max:15', 'regex:/^\d{4}-\d{4}$/'],
+            'telefono_celular' => ['required', 'string', 'max:15', 'regex:/^\d{4}-\d{4}$/'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed', 'regex:/^(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/'],
+            'pais_id' => ['nullable', 'exists:pais,id'],
+            'agree_terms_and_conditions' => ['required'],
+        ], [
+            'numero_identidad.unique' => 'Este número de identidad ya se encuentra registrado',
+            'numero_colegiacion.unique' => 'Este número de colegiación ya se encuentra registrado',
+            'rtn.unique' => 'Este RTN ya se encuentra registrado',
+            'email.unique' => 'Este correo electrónico ya se encuentra registrado',
+            'email.confirmed' => 'La confirmación del correo electrónico no coincide',
+            'password.regex' => 'La contraseña debe contener al menos un símbolo o carácter especial, como por ejemplo: ^?=.,[]{}()!@#$%^&*"|<:>\ ',
+            'telefono.regex' => 'El número de teléfono fijo debe contener solo números y guiones',
+            'telefono_celular.regex' => 'El número de celular debe contener solo números y guiones',
+        ]);
+    }
 
 
     protected function create(array $data)
@@ -81,7 +81,7 @@ class RegisterController extends Controller
             'numero_identidad' => $data['numero_identidad'],
             'numero_colegiacion' => $data['numero_colegiacion'],
             'rtn' => $data['rtn'],
-            'sexo' => $data['sexo'],
+            'sexo_id' => $data['sexo'], // Cambia a 'sexo_id'
             'fecha_nacimiento' => $data['fecha_nacimiento'],
             'telefono' => $data['telefono'],
             'telefono_celular' => $data['telefono_celular'],
